873357331e4f19081330d7722ba8a170767ec9f5
Gsaganichaitanya <chaitanyagasagani@gmail.com>
2023-11-08 16:04:16 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConApp_ass4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Enter a number");
+            number = int.Parse(Console.ReadLine());
+            if (number > 0)
+            {
+                Console.WriteLine("The Number is Positive");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine("The Number is Negative");
+            }
+            else
+            {
+                Console.WriteLine("The Number is Zero");
+            }
+            if(number%2==0)
+            {
+                Console.WriteLine("The number is even");
+            }
+            else
+            {
+                Console.WriteLine("The Number is Odd");
+            }
+            Console.ReadKey();
+        }
+    }
+}
